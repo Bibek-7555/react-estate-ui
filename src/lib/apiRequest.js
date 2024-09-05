0071600a6adabb19de1bf4ea860a7cd756a1df4f
Bibek-7555 <bibekswain7555@gmail.com>
2024-09-05 13:07:57 +0530

    fixed apiRequest

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiRequest = axios.create({
-    baseURL: "https://raelestbackend-1.onrender.com",
+    baseURL: "https://raelestbackend-1.onrender.com/api",
     withCredentials: true
 });
 
